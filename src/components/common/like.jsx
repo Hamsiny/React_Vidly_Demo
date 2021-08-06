a1dfcaef2e784d10122e256d7c46cf81f9e362fb
Hamsiny <459516309@qq.com>
2021-08-06 23:22:38 +1200

    update handle like function

---
+++ src/components/common/like.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const Like = (props) => {
+    return (
+        <i className={props.liked ? "fa fa-heart" : "fa fa-heart-o"}
+            onClick={props.onClick}
+            style={{ cursor: "pointer" }}
+            aria-hidden="true" />
+    )
+}
+export default Like
