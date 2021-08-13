4c4474452fd7b7e9e7810cb906077f2edea7d4a1
Hamsiny <459516309@qq.com>
2021-08-13 22:13:10 +1200

    update component default props value

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -6,13 +6,18 @@ const ListGroup = (props) => {
     <React.Fragment>
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item[textProperty]} className="list-group-item">
-            {item[valueProperty]}
+          <li key={item[valueProperty]} className="list-group-item">
+            {item[textProperty]}
           </li>
         ))}
       </ul>
     </React.Fragment>
   );
+};
+
+ListGroup.defaultProps = {
+    textProperty: "name",
+    valueProperty: "_id",
 };
 
 export default ListGroup;
