3528fb97501d8641003364b99e5bb9c25cd2a296
Hamsiny <459516309@qq.com>
2021-08-19 22:16:01 +1200

    update whole table to a reusable component, update movies component structure

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -46,15 +46,9 @@ export class Movies extends Component {
     this.setState({ sortColumn });
   };
 
-  render() {
-    const { length: count } = this.state.movies;
+  getPagedData = () => {
     const { currentPage, pageSize, sortColumn, selectedGenre, movies } =
       this.state;
-
-    if (count === 0) {
-      return <p>There are no movies in the database.</p>;
-    }
-
     const filtered =
       selectedGenre && selectedGenre._id !== "allGenres"
         ? movies.filter((m) => m.genre._id === selectedGenre._id)
@@ -62,6 +56,19 @@ export class Movies extends Component {
 
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
     const moviesToShow = paginate(sorted, currentPage, pageSize);
+
+    return {totalCount: filtered.length, data: moviesToShow};
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    const { currentPage, pageSize, sortColumn } = this.state;
+
+    if (count === 0) {
+      return <p>There are no movies in the database.</p>;
+    }
+
+    const {totalCount, data: moviesToShow} = this.getPagedData();
 
     return (
       <div className="row">
@@ -73,7 +80,7 @@ export class Movies extends Component {
           />
         </div>
         <div className="col">
-          <p>Showing {filtered.length} movies in the database.</p>
+          <p>Showing {totalCount} movies in the database.</p>
           <MoviesTable
             moviesToShow={moviesToShow}
             sortColumn={sortColumn}
@@ -82,7 +89,7 @@ export class Movies extends Component {
             onSort={this.handleSort}
           />
           <Pagination
-            itemsCount={filtered.length}
+            itemsCount={totalCount}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.handlePageChange}
